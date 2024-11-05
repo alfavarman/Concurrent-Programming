0c07992a6529d4f883b11e3c8ae0e8e452a2d6df
CK-S-ki <communicate2charles@gmail.com>
2024-11-06 00:36:07 +0700

    python 3.12 consisten results, while 3.8 not

--- s002_threads_rest_calls/chars_counter.py
+++ s002_threads_rest_calls/chars_counter.py
@@ -1,6 +1,9 @@
 import json
 import urllib.request
 import time
+from threading import Thread
+
+finished_count = 0
 
 
 def count_letters(url, frequency):
@@ -10,6 +13,8 @@ def count_letters(url, frequency):
         letter = l.lower()
         if letter in frequency:
             frequency[letter] += 1
+    global finished_count
+    finished_count += 1
 
 
 def main():
@@ -17,8 +22,10 @@ def main():
     for c in "abcdefghijklmnopqrstuvwxyz":
         frequency[c] = 0
     start = time.time()
-    for i in range(1000, 1020):
-        count_letters(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)
+    for i in range(1000, 1040):
+        Thread(target=count_letters, args=(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)).start()
+    while finished_count < 40:
+        time.sleep(0.5)
     end = time.time()
     print(json.dumps(frequency, indent=4))
     print("Done, time taken", end - start)
